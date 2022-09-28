6dbf21ef17d7a5d059dfcd9cf518bf2dcc6c9942
hai.nt <hai.nt@novaintechs.com>
2022-09-29 01:51:16 +0700

    mod

---
+++ patch.sh
@@ -0,0 +1,9 @@
+# /usr/core/src/etc/config.xml.sample
+# /usr/core/src/opnsense/mvc/app/models/OPNsense/Core/Menu/Menu.xml
+# /usr/core/src/opnsense/version/core.in
+# /usr/core/Makefile
+# /usr/core/src/opnsense/service/templates/OPNsense/Auth/motd.in
+
+# /usr/core/src/opnsense/www/themes/opnsense/build/images/favicon.png
+# /usr/core/src/opnsense/www/themes/opnsense/build/images/icon-logo.svg
+# /usr/core/src/opnsense/www/themes/opnsense/build/images/default-logo.svg
